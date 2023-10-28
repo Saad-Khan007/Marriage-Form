3d5f1dbd97fb43bda171d3a5497577b8a2bedce4
Saad-Khan007 <saadkhan322417@gmail.com>
2023-10-29 00:19:04 +0500

    form made 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shadi-Form';
+  marriageForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.marriageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      fatherName: ['', Validators.required],
+      fatherOccupation: [''],
+      gender: ['', Validators.required],
+      maritalStatus: ['Single', Validators.required],
+      sons: [0, Validators.required],
+      daughters: [0, Validators.required],
+      lifeStyle: [''],
+      height: ['', Validators.required],
+      age: [18, Validators.required],
+      color: ['', Validators.required],
+      bloodType: [''],
+      haveBeard: ['Yes'],
+      disabilities: [''],
+      qualification: ['', Validators.required],
+      islamicEducation: ['Yes'],
+      parentsAlive: ['Both', Validators.required],
+      brothers: [0, Validators.required],
+      marriedBrothers: [0, Validators.required],
+      sisters: [0, Validators.required],
+      marriedSisters: [0, Validators.required],
+      caste: ['', Validators.required],
+      religion: ['', Validators.required],
+      sect: ['', Validators.required],
+      live_in_country: [''],
+      live_in_state: [''],
+      live_in_city: [''],
+      from_country: [''],
+      from_state: [''],
+      from_city: [''],
+      postalAddress: [''],
+      houseStatus: [''],
+      requirements: ['', Validators.required],
+      profession: ['', Validators.required],
+      designation: ['', Validators.required],
+      monthlyIncome: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // if (this.marriageForm.valid) {
+      console.log(this.marriageForm.value);
+    // }
+  }
 }
